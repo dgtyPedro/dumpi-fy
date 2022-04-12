5655efd89cb669441e2b81345e719cffd61b3169
dgtyPedro <pedrogcs0104@gmail.com>
2022-04-12 10:56:59 -0300

    change title

--- pages/create.php
+++ pages/create.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
-    <title>Create</title>
+    <title>dumpi-fy</title>
     <script src="https://kit.fontawesome.com/b0c45caebd.js" crossorigin="anonymous"></script>
     <link rel="icon" href="./img/logo.png">
 </head>
